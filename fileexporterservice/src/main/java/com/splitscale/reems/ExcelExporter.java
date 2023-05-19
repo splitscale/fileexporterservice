9ad5b41a528fede0c7907fd9931347b273f5f4f9
Yui187 <steven.ballaret-17@cpu.edu.ph>
2023-05-19 18:45:31 +0800

    added excelexporterservice

--- fileexporterservice/src/main/java/com/splitscale/reems/ExcelExporter.java
+++ fileexporterservice/src/main/java/com/splitscale/reems/ExcelExporter.java
@@ -1,31 +1,32 @@
 package com.splitscale.reems;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelExporter {
 
-  public static void exportTableToExcel(String[][] tableData, String filePath) {
-    try (Workbook workbook = new XSSFWorkbook()) {
-      Sheet sheet = workbook.createSheet("Table Data");
+  public static Workbook exportToExcel(List<List<Object>> data)
+    throws IOException {
+    Workbook workbook = new XSSFWorkbook();
+    Sheet sheet = workbook.createSheet("Sheet1");
 
-      int rowCount = 0;
-      for (String[] rowData : tableData) {
-        Row row = sheet.createRow(rowCount++);
-        int columnCount = 0;
-        for (String cellData : rowData) {
-          Cell cell = row.createCell(columnCount++);
-          cell.setCellValue(cellData);
+    int rowNum = 0;
+    for (List<Object> rowData : data) {
+      Row row = sheet.createRow(rowNum++);
+      int colNum = 0;
+      for (Object field : rowData) {
+        Cell cell = row.createCell(colNum++);
+        if (field instanceof String) {
+          cell.setCellValue((String) field);
+        } else if (field instanceof Integer) {
+          cell.setCellValue((Integer) field);
+        } else if (field instanceof Double) {
+          cell.setCellValue((Double) field);
         }
       }
-
-      try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
-        workbook.write(outputStream);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
     }
+    return workbook;
   }
 }
